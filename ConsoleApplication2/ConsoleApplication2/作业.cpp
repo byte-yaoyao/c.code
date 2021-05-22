2f103c3496e96256493fea0a4bc20936040ad265
byte-yaoyao <2282580415@qq.com>
2021-05-22 22:25:19 +0800

    测试代码

---
+++ ConsoleApplication2/ConsoleApplication2/作业.cpp
@@ -0,0 +1,27 @@
+//#include <stdio.h>
+/*int num = 10;
+int main()
+{
+	int num = 1;
+	printf("num = %d\n", num);
+	return 0;
+}*/
+
+/*#include <string.h>
+int main()
+
+{
+	char arr[] ={ 'b', 'i', 't' };
+	printf("%d\n", strlen(arr));
+	return 0; 
+}*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char arr[] = { 'b', 'i', 't' };
+	printf("%d\n", strlen(arr));
+	return 0;
+}
